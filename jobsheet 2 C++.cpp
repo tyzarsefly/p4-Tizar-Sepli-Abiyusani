5bd10312792ee70b05759316da062e0069bab219
tyzarsefly <64633255+tyzarsefly@users.noreply.github.com>
2020-05-11 00:42:55 -0700

    Create jobsheet 2 C++.cpp
    Buatlah  program mengisi dan menampilkan elemen array dengan index array sesuai dengan  NIM terakhir setiap mahasiswa.

---
+++ jobsheet 2 C++.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"Tizar Sepli Abiyusani(F1B019138)-kel.26\n\n";
+	int nim[5];
+	cout<<"Masukkan NIM 1:";
+	cin>>nim[0];
+	cout<<"\nMasukkan NIM 2:";
+	cin>>nim[1];
+	cout<<"\n Masukkan NIM 3:";
+	cin>>nim[2];
+	cout<<"\nMasukkan NIM 4:";
+	cin>>nim[3];
+	cout<<"\nMasukkan NIM 5:";
+	cin>>nim[4];
+	cout<<"\n\nNIM ke-1 ="<<nim[0];
+	cout<<"\nNIM ke-2 ="<<nim[1];
+	cout<<"\nNIM ke-3 ="<<nim[2];
+	cout<<"\nNIM ke-4 ="<<nim[3];
+	cout<<"\nNIM ke-5 ="<<nim[4];
+	return 0;
+	
+}
