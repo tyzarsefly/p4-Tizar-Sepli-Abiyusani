cfddf3aae9d91adf43b3ca2722e4692157134e7b
tyzarsefly <64633255+tyzarsefly@users.noreply.github.com>
2020-05-11 00:30:17 -0700

    Create Jobsheet 1 C.cpp
    Buatlah program array satu dimensi yang menmapilkan masing masing nim mahasiswa

---
+++ Jobsheet 1 C.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main(){
+	int  nim[5]={9,9,7,8,3};
+	printf("Tizar Sepli A. F1B019138 KLP 26");
+	printf("\Nim ke-1 adalah %d",nim[0]);
+	printf("\nNim ke-2 adalah %d",nim[1]);
+	printf("\nNim ke-3 adalah %d",nim[2]);
+	printf("\nNim ke-4 adalah %d",nim[3]);
+	printf("\nNim ke-5 adalah %d",nim[4]);
+	
+}
