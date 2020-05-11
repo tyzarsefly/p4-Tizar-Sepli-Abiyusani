e532f9a9991009d60d8f30d2ecd52c0f350d667f
tyzarsefly <64633255+tyzarsefly@users.noreply.github.com>
2020-05-11 00:51:06 -0700

    Create jobsheet 4 C.cpp
    Buatlah  program  penjumlahan matriks dua dimensi dengan menggunakan NIM dua orang mahasiswa

---
+++ jobsheet 4 C.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <conio.h>
+
+main (){
+	int nim1 [2][2]; 
+	int nim2 [2][2]; 
+	int hasil [2][2];
+	int i, j;
+		printf("Tizar Sepli Abiyusani(F1B019138)-kel.26\n\n");
+	printf ("Pengurangan NIM\n");
+	for (x = 0; x < 2; x++){
+		for (y = 0; y < 2; y++){
+			printf ("Masukkan nilai index %d, %d = ", x, y);
+			scanf ("%d", &nim1[x][y]);
+		}
+	}
+	printf ("\n");
+	for (x = 0; x < 2; x++){
+		for (y = 0; y < 2; y++){
+			printf ("Masukkan nilai index %d, %d = ", x, y);
+			scanf ("%d", &nim2[x][y]);
+		}
+	}
+	for (x = 0; x < 2; x++){
+		for (y = 0; y < 2; y++){
+			hasil [x][y]=nim1[x][y]-nim2[x][y];
+			printf ("%d ", hasil[x][y]);
+		}
+		printf ("\n");
+	}
+	getch();
+}
